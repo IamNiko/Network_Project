163409948b3bd2555c14a3f24470d0966d08d87d
Nicolas Gentile Rosello <iamniko@localhost.localdomain>
2025-05-11 16:06:36 +0200

    Se agrego pagina de inicio, registro de logs de registro e inicio de sesion

--- server/app.py
+++ server/app.py
@@ -1,50 +1,80 @@
-from flask import Flask, request, jsonify
-from models import db, User
+from flask import Flask, request, jsonify, redirect, url_for, render_template, send_from_directory
+from models import db, User, ClientLog
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clients.db'
+
+BASE_DIR=os.path.dirname(os.path.abspath(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'clients.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
 	if request.method == "GET":
-		return "<h2> Formulario de login (en construccion) </h2>"
-	else:
-		data = request.get_json()
-		username = data.get("Username")
-		password = data.get("Password")
+		return render_template("register.html")
 
-		if not username or not password:
-			return jsonify({"ERROR": "Faltan campos"}), 400
+	username=request.form["username"]
+	password=request.form["password"]
 
-		existing_user = User.query.filter_by(username=username).first()
-		if existing_user:
-			return jsonify({"ERROR": "Usuario exitente"}), 400
+	existing_user = User.query.filter_by(username=username).first()
 
-		user = User(username=username, password=password)
-		db.session.add(user)
-		db.session.commit()
+	if existing_user:
+		return "El usuario ya existe"
 
-		return jsonify({"MESSAGE": "Usuario registrado con exito"}), 201
+	new_user = User(username=username, password=password)
+	db.session.add(new_user)
+	db.session.commit()
+
+	log = ClientLog(
+		action="register",
+		username=username,
+		ip_address=request.remote_addr
+	)
+	db.session.add(log)
+	db.session.commit()
 	
+	return render_template("success.html")
 
 
-@app.route("/login", methods=["POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-	data = request.get_json()
-	username = data.get("username")
-	password = data.get("password")
+	if request.method == "GET":
+		return render_template("login.html")
+
+	username=request.form["username"]
+	password=request.form["password"]
 
 	user = User.query.filter_by(username=username, password=password).first()
-	if not user:
-		return jsonify({"ERROR": "Credenciales Incorrectas"}), 401
+	
+	if user:
+		log = ClientLog(
+			action="login",
+			username=username,
+			ip_address=request.remote_addr
+		)
+		db.session.add(log)
+		db.session.commit()
 
-	return jsonify({"MESSAGE": "Login Successful"}), 200
+		return f"<h2>WELCOME, {username}!!!</h2><p>Login Exitoso.</p>"
+
+	else:
+		log = ClientLog(
+			action="Fail",
+			username=username,
+			ip_address=request.remote_addr
+		)
+		db.session.add(log)
+		db.session.commit()
+
+		return "<h2>Credenciales incorrectas</h2><a href='/login'><button>Reintentar</button></a>", 401
+
 
 @app.route("/", methods=["GET"])
 def index():
-	return "<h1> Servidor activo, PEPIN!</h1>"	
+	return render_template("index.html")	
 
 @app.route("/favicon.ico")
 def favicon():
--- server/models.py
+++ server/models.py
@@ -1,16 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
-
-class ClientLog(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	hostname = db.Column(db.String(100), nullable=False)
-	client_ip = db.Column(db.String(100))
-	timestamp = db.Column(db.DateTime, nullable=False)
-	message = db.Column(db.String(200))
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(50), unique=True, nullable=False)
 	password = db.Column(db.String(100), nullable=False)
 	
+class ClientLog(db.Model):
+	id  = db.Column(db.Integer, primary_key=True)
+	action = db.Column(db.String(50), nullable=False)
+	username = db.Column(db.String(50), nullable=False)
+	ip_address = db.Column(db.String(50), nullable=False)
+	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
